e416c6abd433f5003e7b454a50be31aba9153eaa
newbiecoder1331 <81946962+newbiecoder1331@users.noreply.github.com>
2021-10-01 20:37:05 +0530

    add Pramod as a member

--- src/members.js
+++ src/members.js
@@ -5,4 +5,8 @@ export default [
     linkedin: 'https://linkedin.com/in/riteshsp2000',
     portfolio: 'https://riteshpatil.dev',
   },
+  {
+    github: 'newbiecoder1331',
+    linkedin: 'https://linkedin.com/in/pramod-goyal-998048218/',
+  }
 ];
